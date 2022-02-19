7153e540b32f527dfa5b2daaf0dfc273166fdbc8
Rondineli <viajante_666@hotmail.com>
2022-02-19 19:59:15 -0300

    resolvendo exr01 e adicinando indice no readme.txt

---
+++ Exr01/Ex01.java
@@ -0,0 +1,11 @@
+// 1) Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+
+
+public class Ex01{
+	public static void main (String[] args){
+		
+		System.out.println();
+		System.out.println("Ola Mundo");
+		System.out.println();
+	}
+}
